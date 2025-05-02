17f7e3aa213d3b5ec834062a4f9217df1868ec60
Rizwan-Ghouri <rizwanidrees138@gmail.com>
2025-05-02 10:37:54 +0500

    simple changes

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -19,9 +19,9 @@ export default function Project() {
                 </div>
                 <div className="lg:m-2 lg:w-2/5 lg:border-2 lg:rounded-xl lg:px-5 lg:py-5 md:w-3/4 md:block md:p-5 md:m-20 md:border-2 sm:w-3/4 sm:block sm:p-6 sm:m-16 sm:border-2 min-[300px]:w-2/4 max-[300px]:border-2 min-[300px]:m-20" >
                     <img className=" w-full h-52 rounded-md" src="https://plus.unsplash.com/premium_photo-1718747305165-32c4012b2962?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
-                    <h1 className="p-1 text-2xl text-yellow-500">iCoder Responsive Website</h1>
+                    <h1 className="p-1 text-2xl text-yellow-500">all-task</h1>
                     <p className="my-5">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat labore incidunt rem voluptatem, veniam accusamus facere! Veniam dignissimos deserunt, explicabo numquam distinctio ad harum facilis architecto. Magnam nostrum quasi labore.</p>
-                    <Link className="w-28 p-1 text-xl rounded-xl bg-yellow-500" to={"https://rizwan-ghouri.github.io/iCoderBootstrap/"} target="_blank">Demo</Link>
+                    <Link className="w-28 p-1 text-xl rounded-xl bg-yellow-500" to={"all-task-one.vercel.app"} target="_blank">Demo</Link>
                 </div>
             </section>
             <section className="lg:w-full lg:flex lg:space-x-5 p-5 text-white sm:block">
@@ -29,7 +29,7 @@ export default function Project() {
                     <img className=" w-full h-52 rounded-md" src="../src/image/resposive web.jpg" alt="" />
                     <h1 className="p-1 text-2xl text-yellow-500">Text.Editor</h1>
                     <p className="my-5">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat labore incidunt rem voluptatem, veniam accusamus facere! Veniam dignissimos deserunt, explicabo numquam distinctio ad harum facilis architecto. Magnam nostrum quasi labore.</p>
-                    <Link className="w-28 p-1 text-xl rounded-xl bg-yellow-500" to={"https://rizwan-ghouri.github.io/Text.Editor/"} target="_blank">Demo</Link>
+                    <Link className="w-28 p-1 text-xl rounded-xl bg-yellow-500" to={"https://rizwan-ghouri.github.io/Age-Calculator/"} target="_blank">Demo</Link>
                 </div>
                 <div className="lg:m-2 lg:w-2/5 lg:border-2 lg:rounded-xl lg:px-5 lg:py-5 md:w-3/4 md:block md:p-5 md:m-20 md:border-2 sm:w-3/4 sm:block sm:p-6 sm:m-16 sm:border-2 min-[300px]:w-2/4 max-[300px]:border-2 min-[300px]:m-20" >
                     <img className=" w-full h-52 rounded-md" src="../src/image/resposive web1.jpg" alt="" />

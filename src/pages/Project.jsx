8ec4aa6675305c9295f230821231e9b133cea760
Rizwan-Ghouri <rizwanidrees138@gmail.com>
2025-05-02 10:31:45 +0500

    G-A project

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -6,13 +6,13 @@ export default function Project() {
             <h1 className="p-2 text-3xl text-center text-yellow-500 md:text-center">Project</h1>
             <section className="lg:w-full lg:flex lg:space-x-5 p-5 text-white sm:block">
                 <div className="lg:m-2 lg:w-2/5 lg:border-2 lg:rounded-xl lg:px-5 lg:py-5 md:w-3/4 md:block md:p-5 md:m-20 md:border-2 sm:w-3/4 sm:block sm:p-6 sm:m-16 sm:border-2 min-[300px]:w-2/4 max-[300px]:border-2 min-[300px]:m-20" >
-                    <img className=" w-full h-52 rounded-md" src="../src/image/resposive web.jpg" alt="" />
+                    <img className=" w-full h-52 rounded-md" src="/resposive web.jpg" alt="" />
                     <h1 className="p-1 text-2xl text-yellow-500">Text.Editor</h1>
                     <p className="my-5">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat labore incidunt rem voluptatem, veniam accusamus facere! Veniam dignissimos deserunt, explicabo numquam distinctio ad harum facilis architecto. Magnam nostrum quasi labore.</p>
                     <Link className="w-28 p-1 text-xl rounded-xl bg-yellow-500" to={"https://rizwan-ghouri.github.io/Text.Editor/"} target="_blank">Demo</Link>
                 </div>
                 <div className="lg:m-2 lg:w-2/5 lg:border-2 lg:rounded-xl lg:px-5 lg:py-5 md:w-3/4 md:block md:p-5 md:m-20 md:border-2 sm:w-3/4 sm:block sm:p-6 sm:m-16 sm:border-2 min-[300px]:w-2/4 max-[300px]:border-2 min-[300px]:m-20" >
-                    <img className=" w-full h-52 rounded-md" src="../src/image/resposive web1.jpg" alt="" />
+                    <img className=" w-full h-52 rounded-md" src="/resposive web1.jpg" alt="" />
                     <h1 className="p-1 text-2xl text-yellow-500">iCoder Responsive Website</h1>
                     <p className="my-5">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat labore incidunt rem voluptatem, veniam accusamus facere! Veniam dignissimos deserunt, explicabo numquam distinctio ad harum facilis architecto. Magnam nostrum quasi labore.</p>
                     <Link className="w-28 p-1 text-xl rounded-xl bg-yellow-500" to={"https://rizwan-ghouri.github.io/iCoderBootstrap/"} target="_blank">Demo</Link>
